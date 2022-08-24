d1bb4a411b9bbabb9e366ae029c7f3042e677ad3
KannuSingh <karandeepsinghcaj@outlook.com>
2022-08-23 20:20:33 -0400

    fixed artifacts location in relay

--- apps/relay/src/controllers/trueFantasySportsArtifacts.controllers.ts
+++ apps/relay/src/controllers/trueFantasySportsArtifacts.controllers.ts
@@ -4,7 +4,7 @@ const path = require("path")
 //route to download a file
 const downloadSnarkArtifacts = (_req: express.Request, _res: express.Response) => {
     var file = _req.params.file
-    var fileLocation = path.join("../../uploads", file)
+    var fileLocation = path.join("./uploads", file)
     console.log(fileLocation)
     _res.download(fileLocation, file)
 }
